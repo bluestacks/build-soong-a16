248724fd58948e0f098b632f5a00bc4b931268de
mark-bst <zeqing.xu@bluestacks.com>
2026-07-29 09:49:41 +0800

    [A16] Revert "build/soong: drop ../ggl/goldfish-opengl-pie from outsideModList scan (android-16 25Q4 gfxstream guest already provides libqemupipe.ranchu etc.; was duplicate). Keep ../hd/Source/* (BST hd guest modules)."
    This reverts commit daa9ccd3ea4ce7875206975e875345ee67fc4153.

--- ui/build/finder.go
+++ ui/build/finder.go
@@ -149,6 +149,7 @@ func FindSources(ctx Context, config Config, f *finder.Finder) {
 		"../hd/Source/tools/bstchkdata/Android.mk",
 		"../hd/Source/hcall/guest/Android.mk",
 		"../hd/Source/gcall/guest/Android.mk",
+		"../ggl/goldfish-opengl-pie/Android.mk",
 	}
 	for _, outsideMod := range outsideModList {
 		androidMks = append(androidMks, outsideMod)
